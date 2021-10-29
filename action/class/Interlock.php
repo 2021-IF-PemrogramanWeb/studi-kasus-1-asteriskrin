92dc57e70437a2b1f848b602186e61f7a1ae56a3
Ryan Garnet Andrianto <ryangarnetandrianto@gmail.com>
2021-10-29 16:28:24 +0700

    Menampilkan data tabel dari DB

---
+++ action/class/Interlock.php
@@ -0,0 +1,41 @@
+<?php
+    class Interlock {
+        public function getReason($id) {
+            $reason = "Unknown";
+            switch ($id) {
+                case 1: $reason = "Interlock Hose Reel Front"; break;
+                case 2: $reason = "Interlock Hose Reel Rear"; break;
+                case 3: $reason = "Interlock Input Coupler Stow"; break;
+                case 4: $reason = "Interlock Input Hose Boom Stow"; break;
+                case 5: $reason = "Interlock Platform Stow"; break;
+                case 6: $reason = "Interlock Platform Nozzle Left"; break;
+                case 7: $reason = "Interlock Platform Nozzle Right"; break;
+                case 8: $reason = "Interlock Boom Stow"; break;
+                case 9: $reason = "Interlock Bonding Static Reel Front"; break;
+                case 10: $reason = "Interlock Bonding Static Reel Rear"; break;
+                case 11: $reason = "Interlock Bottom Loading"; break;
+                case 12: $reason = "Interlock Handrail"; break;
+                case 13: $reason = "PTO"; break;
+                case 14: $reason = "Preventive Maintenance"; break;
+                case 15: $reason = "Interlock System Fault"; break;
+                case 16: $reason = "Breakdown"; break;
+            }
+            return $reason;
+        }
+        public function getData() {
+            include 'action/conn.php';
+            $query = "SELECT i.i_id, i.i_timeon, i.i_timeoff, u.u_username, i.i_reasonid FROM interlocks i, users u WHERE i.i_ack = u.u_id";
+    
+            $result = $conn->query($query);
+            if ($result->num_rows > 0) {
+                // output data of each row
+                $data = [];
+                while($row = $result->fetch_assoc()) {
+                    array_push($data, $row);
+                }
+            }
+            $conn->close();
+            return $data;
+        }
+    }
+?>
---
+++ page1.php
@@ -0,0 +1,73 @@
+<?php
+    include 'action/class/Interlock.php';
+    $interlocks = new Interlock();
+    $interlock_datas = $interlocks->getData();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Page 1</title>
+</head>
+<body>
+    <div class="d-flex flex-column p-4 content">
+        <div class="d-flex">
+            <img src="img/logo.png" class="flex-grow-0">
+            <div class="flex-grow-1">
+                <button class="btn btn-dark float-end">Tgl Hari ini</button>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-sm-12 col-md-3">
+                <div class="d-flex flex-column h-100">
+                    <div class="flex-grow-1 align-self-center">
+                        <div class="rounded bg-blue">&nbsp;</div>
+                        Mobil 1
+                        <div class="rounded bg-green">&nbsp;</div>
+                        Mobil 2
+                    </div>
+                    <div class="align-self-center d-flex flex-column justify-content-center gap-4">
+                        <a href="grafik.html" class="btn bg-yellow">Graph</a>
+                        <a class="btn bg-blue-2">Export</a>
+                        <a class="btn bg-red">Logout</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-9">
+                <table class="table table-bordered border-dark">
+                    <thead>
+                        <tr style="background-color: rgb(168, 209, 141);">
+                            <th>No</th>
+                            <th>On</th>
+                            <th>Off</th>
+                            <th>Ack by</th>
+                            <th>Reason</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($interlock_datas as $d) { ?>
+                        <tr>
+                            <td><?php echo $d['i_id']; ?></td>
+                            <td><?php echo $d['i_timeon']; ?></td>
+                            <td><?php echo $d['i_timeoff']; ?></td>
+                            <td>
+                                Act: <?php echo $d['u_username']; ?><br>
+                                Dis: Rizaldy, Gathot
+                            </td>
+                            <td>
+                                <?php echo $d['i_reasonid'].'. '.$interlocks->getReason($d['i_reasonid']); ?>
+                            </td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <script src="bootstrap/bootstrap.min.js"></script>
+</body>
+</html>
